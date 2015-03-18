eddcd34dd17b12fea19202056c7436c835826059
Ben Clayton <headlessclayton@gmail.com>
2015-03-18 15:52:43 +0000

    Add a sample for panels and splitters

---
+++ samples/panels/main.go
@@ -0,0 +1,71 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+
+	"github.com/google/gxui"
+	"github.com/google/gxui/drivers/gl"
+	"github.com/google/gxui/themes/dark"
+)
+
+var data = flag.String("data", "", "path to data")
+
+// Create a PanelHolder with a 3 panels
+func panelHolder(name string, theme gxui.Theme) gxui.PanelHolder {
+	label := func(text string) gxui.Label {
+		label := theme.CreateLabel()
+		label.SetText(text)
+		return label
+	}
+
+	holder := theme.CreatePanelHolder()
+	holder.AddPanel(label(name+" 0 content"), name+" 0 panel")
+	holder.AddPanel(label(name+" 1 content"), name+" 1 panel")
+	holder.AddPanel(label(name+" 2 content"), name+" 2 panel")
+	return holder
+}
+
+func appMain(driver gxui.Driver) {
+	theme := dark.CreateTheme(driver)
+
+	// ┌───────┐║┌───────┐
+	// │       │║│       │
+	// │   A   │║│   B   │
+	// │       │║│       │
+	// └───────┘║└───────┘
+	// ═══════════════════
+	// ┌───────┐║┌───────┐
+	// │       │║│       │
+	// │   C   │║│   D   │
+	// │       │║│       │
+	// └───────┘║└───────┘
+
+	splitterAB := theme.CreateSplitterLayout()
+	splitterAB.SetOrientation(gxui.Horizontal)
+	splitterAB.AddChild(panelHolder("A", theme))
+	splitterAB.AddChild(panelHolder("B", theme))
+
+	splitterCD := theme.CreateSplitterLayout()
+	splitterCD.SetOrientation(gxui.Horizontal)
+	splitterCD.AddChild(panelHolder("C", theme))
+	splitterCD.AddChild(panelHolder("D", theme))
+
+	vSplitter := theme.CreateSplitterLayout()
+	vSplitter.SetOrientation(gxui.Vertical)
+	vSplitter.AddChild(splitterAB)
+	vSplitter.AddChild(splitterCD)
+
+	window := theme.CreateWindow(800, 600, "Panels")
+	window.AddChild(vSplitter)
+	window.OnClose(driver.Terminate)
+	gxui.EventLoop(driver)
+}
+
+func main() {
+	flag.Parse()
+	gl.StartDriver(*data, appMain)
+}
